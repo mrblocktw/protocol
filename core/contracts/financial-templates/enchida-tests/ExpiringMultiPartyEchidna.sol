163175c59b2d4c65df29e4e418bacd8fe68600cb
Christopher Maree <christopher.maree@gmail.com>
2020-03-12 10:46:04 +0200

    Added initial enchida test
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/contracts/financial-templates/enchida-tests/ExpiringMultiPartyEchidna.sol
+++ core/contracts/financial-templates/enchida-tests/ExpiringMultiPartyEchidna.sol
@@ -1,1 +1,10 @@
-    
+pragma solidity ^0.6.0;
+
+import "../implementation/ExpiringMultiParty";
+
+contract ExpiringMultiPartyEchinda is ExpiringMultiParty {
+
+    function echidna_assert() public returns (bool) {
+        return true;
+    }
+}
