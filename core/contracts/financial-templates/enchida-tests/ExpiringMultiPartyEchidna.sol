50689ceff100f2a14d07b7acc8cd455319a15bb6
Christopher Maree <christopher.maree@gmail.com>
2020-03-12 10:47:02 +0200

    Lint
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/contracts/financial-templates/enchida-tests/ExpiringMultiPartyEchidna.sol
+++ core/contracts/financial-templates/enchida-tests/ExpiringMultiPartyEchidna.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.6.0;
 
 import "../implementation/ExpiringMultiParty";
 
-contract ExpiringMultiPartyEchinda is ExpiringMultiParty {
 
+contract ExpiringMultiPartyEchinda is ExpiringMultiParty {
     function echidna_assert() public returns (bool) {
         return true;
     }
