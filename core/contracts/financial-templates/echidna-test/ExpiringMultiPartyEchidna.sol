271192afddfcc5e7d59b03772a7aad648956010e
Christopher Maree <christopher.maree@gmail.com>
2020-03-27 12:38:45 +0200

    updated tests
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/contracts/financial-templates/echidna-test/ExpiringMultiPartyEchidna.sol
+++ core/contracts/financial-templates/echidna-test/ExpiringMultiPartyEchidna.sol
@@ -1,13 +1,34 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
+// Contract to test
 import "../implementation/ExpiringMultiParty.sol";
 
+// Helper contracts
+import "../../common/implementation/ExpandedERC20.sol";
+import "../implementation/TokenFactory.sol";
 
 contract ExpiringMultiPartyEchinda is ExpiringMultiParty {
-    constructor(ConstructorParams memory params) public ExpiringMultiParty(params) {}
+    ConstructorParams mockParams = ConstructorParams(
+        true,
+        1234567890,
+        1000,
+        address(0),
+        address(0),
+        address(0),
+        bytes32("UMATEST"),
+        "Test UMA Token",
+        "UMATEST",
+        1000,
+        FixedPoint.Unsigned(1500000000000000000),
+        FixedPoint.Unsigned(100000000000000000),
+        FixedPoint.Unsigned(100000000000000000),
+        FixedPoint.Unsigned(100000000000000000)
+    );
 
-    function echidna_assert() public returns (bool) {
-        return true;
+    constructor() public ExpiringMultiParty(mockParams) {}
+
+    function echidna_true() public returns (bool) {
+        return (true);
     }
 }
